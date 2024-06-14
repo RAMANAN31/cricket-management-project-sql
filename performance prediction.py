ba5b11ff0e7a2c091941d704ade893b121345d92
RAMANAN <112418260+RAMANAN31@users.noreply.github.com>
2024-06-14 17:31:03 +0530

    Update performance prediction.py

--- performance prediction.py
+++ performance prediction.py
@@ -1,4 +1,3 @@
-#player prediction usinng ml
 import pandas as pd
 
 # Player table
@@ -6,10 +5,10 @@ player_data = {
     'plr_name': ['John Smith', 'David Warner', 'Rohit Sharma', 'Kane Williamson', 'Virat Kohli', 'AB de Villiers', 'Andre Russell', 'Ben Stokes', 'Shakib Al Hasan', 'Rashid Khan', 'Jasprit Bumrah'],
     'plr_ID': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
     'plr_t_ID': [1001, 1002, 1003, 1001, 1002, 1003, 1001, 1002, 1003, 1001, 1002],
-    'plr_contact': [1234567890, 9876543210, 5555555555, 1111111111, 2222222222, 3333333333, 4444444444, 6666666666, 7777777777, 8888888888, 9999999999],
+    'plr_contact_amt': [1200000, 2300000, 1100000, 3400000, 4300000, 2330000, 1240000, 4240000, 5543000, 1220000, 2220000],
     'contract_start': ['2022-01-01'] * 11,
     'contract_end': ['2023-12-31', '2025-12-31', '2024-12-31', '2023-12-31', '2025-12-31', '2024-12-31', '2023-12-31', '2025-12-31', '2024-12-31', '2023-12-31', '2025-12-31'],
     'plr_type': ['Batsman', 'Batsman', 'Batsman', 'Batsman', 'Batsman', 'Batsman', 'All-rounder', 'All-rounder', 'All-rounder', 'Bowler', 'Bowler']
 }
 df_player = pd.DataFrame(player_data)
-
+df_player 
